1f005b44e3dc63182293a7bcebd1947970ea7851
tanche <>
2025-09-24 17:42:09 +0200

    add c macros to avoid call to stdio (printf and malloc) due to this are not implemented in microzig foundation-libc

--- build.zig
+++ build.zig
@@ -10,9 +10,14 @@ pub fn build(b: *std.Build) void {
         .root_module = b.createModule(.{
             .target = target,
             .optimize = optimize,
-            .link_libc = true,
+            .link_libc = false,
         }),
+        .linkage = .static,
     });
+    littlefs.root_module.addCMacro("LFS_NO_DEBUG", "1");
+    littlefs.root_module.addCMacro("LFS_NO_ERROR", "1");
+    littlefs.root_module.addCMacro("LFS_NO_WARN", "1");
+    littlefs.root_module.addCMacro("LFS_NO_MALLOC", "1");
 
     littlefs.addCSourceFile(.{
         .file = littlefs_dep.path("lfs.c"),
