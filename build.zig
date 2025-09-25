bf94a7ccedba6c719b5a51d8a1814b6c6f20aed1
tanche <>
2025-09-25 21:34:37 +0200

    fix build.zig module export and option

--- build.zig
+++ build.zig
@@ -6,7 +6,8 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     const stdlib = b.option(bool, "stdlib", "link to stdlib") orelse false;
-    const log_en = b.option(bool, "log_enable", "Enable logging function") orelse false;
+    const log_enable = b.option(bool, "log_enable", "Enable logging function") orelse false;
+    const malloc_enable = b.option(bool, "Enable Malloc", "Allowing used of malloc in littlefs") orelse false;
 
     const lfs_lib = b.addLibrary(.{
         .name = "littlefs",
@@ -32,21 +33,24 @@ pub fn build(b: *std.Build) void {
         .link_libc = stdlib,
     });
     lfs_translate.addIncludePath(lfs_dep.path(""));
-    const lfs_translate_mod = lfs_translate.createModule();
+    _ = lfs_translate.addModule("lfs");
 
-    if (log_en == false) {
+    if (log_enable == false) {
         lfs_lib.root_module.addCMacro("LFS_NO_DEBUG", "1");
         lfs_lib.root_module.addCMacro("LFS_NO_WARN", "1");
         lfs_lib.root_module.addCMacro("LFS_NO_ERROR", "1");
-        lfs_lib.root_module.addCMacro("LFS_NO_MALLOC", "1");
 
         lfs_translate.defineCMacro("LFS_NO_DEBUG", "1");
         lfs_translate.defineCMacro("LFS_NO_WARN", "1");
         lfs_translate.defineCMacro("LFS_NO_ERROR", "1");
+    }
+
+    if (malloc_enable == false) {
+        lfs_lib.root_module.addCMacro("LFS_NO_MALLOC", "1");
+
         lfs_translate.defineCMacro("LFS_NO_MALLOC", "1");
     }
 
-    lfs_lib.root_module.addImport("lfs", lfs_translate_mod);
     lfs_lib.installHeader(
         lfs_dep.path("lfs.h"),
         "lfs.h",
